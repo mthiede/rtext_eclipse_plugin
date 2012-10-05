a413f1d805e91b6df2c9c8339053ef601125bb76
Martin Thiede <martin.thiede@gmx.de>
2012-10-05 19:31:10 +0200

    fixed jumping to elements in linked files

--- rtext_plugin/src/org/rtext/editor/OpenEditorHelper.java
+++ rtext_plugin/src/org/rtext/editor/OpenEditorHelper.java
@@ -1,5 +1,7 @@
 package org.rtext.editor;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,35 +9,42 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.ide.IDE;
 
 public class OpenEditorHelper {
-	
-	public static void openInRTextEditor(IWorkbenchPage workbenchPage, String filename, int line) {
-		IPath path = new Path(filename);
-		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(path);
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put(IMarker.LINE_NUMBER, new Integer(line));
-		map.put(IDE.EDITOR_ID_ATTR,	"org.rtext.Editor");
-		IMarker marker = null;
+
+	public static void openInRTextEditor(IWorkbenchPage workbenchPage,
+			String filename, int line) {
+		IFile[] files;
 		try {
-			marker = file.createMarker(IMarker.TEXT);
-			marker.setAttributes(map);
-			IDE.openEditor(workbenchPage, marker);
-		} catch (PartInitException e) {
-		} catch (CoreException e1) {
-		}
-		finally {
-			if (marker != null) {
+			files = ResourcesPlugin.getWorkspace().getRoot().findFilesForLocationURI(
+					new URI("file://"+filename));
+			if (files != null && files.length > 0) {
+				IFile file = files[0];
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put(IMarker.LINE_NUMBER, new Integer(line));
+				map.put(IDE.EDITOR_ID_ATTR, "org.rtext.Editor");
+				IMarker marker = null;
 				try {
-					marker.delete();
-				} catch (CoreException e) {
+					marker = file.createMarker(IMarker.TEXT);
+					marker.setAttributes(map);
+					IDE.openEditor(workbenchPage, marker);
+				} catch (PartInitException e) {
+				} catch (CoreException e1) {
+				} finally {
+					if (marker != null) {
+						try {
+							marker.delete();
+						} catch (CoreException e) {
+						}
+					}
 				}
 			}
+		} catch (URISyntaxException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
 		}
 	}
 }
