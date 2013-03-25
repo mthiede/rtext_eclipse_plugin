6f6000e9b96da58ee9de4ba027de2a7fbbd84eba
Sebastian Benz <sebastian.benz@esrlabs.com>
2013-03-25 08:26:31 +0100

    clean up

--- org.rtext.lang.editor/src/org/rtext/lang/editor/AbstractEditStrategy.java
+++ org.rtext.lang.editor/src/org/rtext/lang/editor/AbstractEditStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+  * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -79,7 +79,7 @@ public abstract class AbstractEditStrategy  implements IAutoEditStrategy, Verify
 		}
 	}
 	
-	/**
+	/** 
 	 * finds the first start terminal which is not closed before the cursor position.
 	 */
 	protected IRegion findStopTerminal(IDocument document, int offset) throws BadLocationException {
